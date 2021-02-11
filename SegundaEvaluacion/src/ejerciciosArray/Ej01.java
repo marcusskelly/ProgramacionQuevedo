6ddb46cb95f35b815433288282a34833a3000de5
Marcus Skelly <marcusskellyburgos@gmail.com>
2021-02-11 13:10:39 +0100

    Update Ej01.java

--- SegundaEvaluacion/src/ejerciciosArray/Ej01.java
+++ SegundaEvaluacion/src/ejerciciosArray/Ej01.java
@@ -20,3 +20,6 @@ public class Ej01 {
     }
 
 }
+/*Realizar un programa que inicialice una lista con 10 valores aleatorios (del 1 al 10) y
+posteriormente muestre en pantalla cada elemento de la lista junto con su cuadrado y su
+cubo.*
