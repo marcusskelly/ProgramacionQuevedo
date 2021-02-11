76647e203d42d28b2e5ac5a7b7f353d61917adb4
Marcus Skelly <marcusskellyburgos@gmail.com>
2021-02-11 13:18:56 +0100

    Update Ej05.java

--- SegundaEvaluacion/src/ejerciciosArray/Ej05.java
+++ SegundaEvaluacion/src/ejerciciosArray/Ej05.java
@@ -18,3 +18,5 @@ public class Ej05 {
         System.out.println(Arrays.toString(inverso));
     }
 }
+/*Hacer un programa que inicialice una lista de números con valores aleatorios (10 valores), y
+posterior ordene los elementos de menor a mayor.
