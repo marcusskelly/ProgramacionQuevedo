089f8268c09fc7b25731749b5246dd7c16dcdb12
Marcus Skelly <marcusskellyburgos@gmail.com>
2021-02-11 14:31:33 +0100

    Create Ej1.java

---
+++ SegundaEvaluacion/src/string/Ej1.java
@@ -0,0 +1,12 @@
+/*Realizar un programa que comprueba si una cadena leída por teclado comienza por una subcadena
+introducida por teclado./*
+
+Scanner sc = new Scanner(System.in);
+System.out.println("Introduce cadena: ");
+String cadena = sc.nextLine();
+System.out.println("Introduce subcadena: ");
+String subcadena = sc.nextLine();
+// Comprueba si cadena comienca por subcadena: método startsWith:
+boolean comprueba = cadena.startsWith(subcadena);
+if (comprueba) System.out.println("Cierto");
+else System.out.println("Falso");
