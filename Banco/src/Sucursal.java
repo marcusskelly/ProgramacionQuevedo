2e40e1fc07d930bb2157696f540c14d6d7d1863f
Marcus Skelly <marcusskellyburgos@gmail.com>
2021-04-26 13:16:59 +0200

    Update Sucursal.java

--- Banco/src/Sucursal.java
+++ Banco/src/Sucursal.java
@@ -1,5 +1,7 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.lang.Math;
 
 public class Sucursal {
 
@@ -11,32 +13,85 @@ public class Sucursal {
         this.entidad = entidad;
         this.oficina = oficina;
         ArrayList<Cuenta> cuentas = new ArrayList<>();
+
+        String ruta = "C:\\Users\\Estela\\IdeaProjects\\ficheros\\Banco";
+        File fichero = new File(ruta + "\\" + entidad + "\\" + oficina);
+        fichero.mkdirs();
     }
 
-    public String generarCC(){
-        int codigoControl1 = (int)(Math.random() * 9);
-        int codigoControl2 = (int)(Math.random() * 9);
-        String cControl = String.valueOf(codigoControl1) + String.valueOf(codigoControl2);
-        return cControl;
+    public String generarCC() {
+
+        int CC1 = (int)(Math.random() * 9);
+        int CC2 = (int)(Math.random() * 9);
+        String codigoControl = String.valueOf(CC1) + String.valueOf(CC2);
+
+        return codigoControl;
     }
 
     public String generarNumeroCuenta(){
-        int numeroCuenta1 = (int)(Math.random() * 99999);
-        int numeroCuenta2 = (int)(Math.random() * 99999);
-        String nCuenta = String.valueOf(numeroCuenta1) + String.valueOf(numeroCuenta2);
-        return nCuenta;
+
+        int cuenta1 = (int)(Math.random() * 99999);
+        int cuenta2 = (int)(Math.random() * 99999);
+        String numeroCuenta = String.valueOf(cuenta1) + String.valueOf(cuenta2);
+
+        return numeroCuenta;
     }
 
     public String generarIban(){
         String iban = "ES" + generarCC() + entidad + oficina + generarCC() + generarNumeroCuenta();
+
         return iban;
     }
 
-    public void crearCuenta(String titular, double saldo){
+    public void crearCuenta(String titular, double saldo)  {
+
         Cuenta cuenta1 = new Cuenta(titular,generarIban(),saldo);
         cuentas.add(cuenta1);
     }
 
-    
+
+    public double ingresar(Cuenta cuenta1,double cantidad) {
+        if (cuenta1.isBloqueada() == false) {
+            cuenta1.setSaldo(cuenta1.getSaldo() + cantidad);
+            String ruta = "C:\\Users\\Estela\\IdeaProjects\\ficheros\\Banco";
+            try (FileWriter fw = new FileWriter(ruta + "\\" + cuenta1.getIban().substring(4,8) + "\\" + cuenta1.getIban().substring(8,12) + "\\" + cuenta1.getIban().substring(14,24) + "\\movimientos.txt", true)) {
+                // append = true -> añade al final
+                // append = false (por defecto) -> sobreescribe
+                String saldoCadena = String.valueOf(cantidad);
+                fw.write(saldoCadena);
+
+            } catch (IOException e) {
+                System.out.println("Excepción de E/S: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Cuenta bloqueada");
+        }
+        return cuenta1.getSaldo();
+    }
+
+    public double retirar(Cuenta cuenta1, double cantidad) {
+        if (cuenta1.isBloqueada() == false) {
+            if (cantidad <= cuenta1.getSaldo()) {
+                cuenta1.setSaldo(cuenta1.getSaldo() - cantidad);
+                System.out.println("Saldo resultante: " + cuenta1.getSaldo());
+                String ruta = "C:\\Users\\Estela\\IdeaProjects\\ficheros\\Banco";
+                try (FileWriter fw = new FileWriter(ruta + "\\" + cuenta1.getIban().substring(4,8) + "\\" + cuenta1.getIban().substring(8,12) + "\\" + cuenta1.getIban().substring(14,24) + "\\movimientos.txt", true)) {
+                    // append = true -> añade al final
+                    // append = false (por defecto) -> sobreescribe
+                    String saldoCadena = "-" + String.valueOf(cantidad);
+                    fw.write(saldoCadena);
+
+                } catch (IOException e) {
+                    System.out.println("Excepción de E/S: " + e.getMessage());
+                }
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        } else {
+            System.out.println("Cuenta bloqueada");
+        }
+        return cuenta1.getSaldo();
+    }
+
 
 }
